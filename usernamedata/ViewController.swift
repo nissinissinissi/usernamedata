9f9e64ba214a221406eacdb878acdc6f17bb0338
nissinissinissi <nissisalamat14@icloud.com>
2022-10-05 13:12:11 -0500

    1

--- usernamedata/ViewController.swift
+++ usernamedata/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    var userData : [String:String] = ["Okeanna":"Okeanna", "123":"321"]
+    @IBAction func SignIn(_ sender: Any) {
+        if userData[userName.text!] != nil {
+            if userData [userName.text!] == password.text!{
+                print ("You sign in!")
+            }else {
+                print ("Incorrect password")
+            }
+        }
+    }
+    @IBAction func SignUp(_ sender: Any) {
+        userData[userName.text!] = password.text
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        userName.layer.cornerRadius = 22
+        password.layer.cornerRadius = 22
+    }
 }
+
 
