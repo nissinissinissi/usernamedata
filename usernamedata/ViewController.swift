49bcb000005574d818cfefb881233bd3bde4f148
nissinissinissi <nissisalamat14@icloud.com>
2022-10-05 10:58:42 -0500

    Initial Commit

---
+++ usernamedata/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  usernamedata
+//
+//  Created by User on 10 Tishri 5783.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
